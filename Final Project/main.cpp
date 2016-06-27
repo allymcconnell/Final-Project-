fe95cc5291caa2c980554e5e7535dc721c3c9dd8
allymcconnell <amcconnell16@my.whitworth.edu>
2016-06-27 09:30:00 -0700

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Ally McConnell on 6/27/16.
+//  Copyright © 2016 Ally McConnell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
